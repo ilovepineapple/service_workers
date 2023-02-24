231bfb531ac91482bf752a6458b3fdb69f89a509
Pineapple <pineappleman@skiff.com>
2023-02-23 21:12:11 -0300

    chore: 🧩 console events

--- src/register_service_worker/events.js
+++ src/register_service_worker/events.js
@@ -1,7 +1,9 @@
 self.addEventListener("install", (event) => {
   console.log("Install the event!");
+  console.log({ event });
 });
 
 self.addEventListener("activate", (event) => {
   console.log("Activate the event!");
+  console.log({ event });
 });
